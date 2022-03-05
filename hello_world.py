dc916a824fb7b6f3c055cbd9a14cb02389bda31a
Christian Ocampo Ruiz <christiano.campo@Christianocampo.local>
2022-03-04 21:11:05 -0600

    Firs try postgres connection

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,30 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.providers.google.cloud.operators.cloud_sql import CloudSQLExecuteQueryOperator
 
 def print_hello():
     return 'Hello world from first Airflow DAG crisonew3!'
 
-dag = DAG('hello_world', description='Hello World DAG',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2017, 3, 20), catchup=False)
+SQL = [
+    'CREATE TABLE IF NOT EXISTS TABLE_TEST (I INTEGER)',
+    'CREATE TABLE IF NOT EXISTS TABLE_TEST (I INTEGER)',  # shows warnings logged
+    'INSERT INTO TABLE_TEST VALUES (0)',
+    'CREATE TABLE IF NOT EXISTS TABLE_TEST2 (I INTEGER)',
+    'DROP TABLE TABLE_TEST',
+    'DROP TABLE TABLE_TEST2',
+]
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+with DAG(
+    dag_id='dag_with_postgres_operator_v01',
+    degault_args = default_args,
+    start_date=datetime(2022,03,03),
+    schedule_interval='0 0 * * *'
+) as dag:
+    task1 = CloudSQLExecuteQueryOperator(
+        task_id='create_postgres_table',
+        gcp_cloudsql_conn_id='google_postgres',
+        sql=SQL
+    )
+    hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
-hello_operator
+hello_operator >> task1
