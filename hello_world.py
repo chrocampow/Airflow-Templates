4bcd90e0c24896a6299af9264edf7a29e3d45b78
Christian Ocampo Ruiz <christiano.campo@Christianocampo.local>
2022-03-04 21:18:56 -0600

    Fourth trye postgres

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,15 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.providers.google.cloud.operators.cloud_sql import CloudSQLExecuteQueryOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 
 def print_hello():
     return 'Hello world from first Airflow DAG crisonew3!'
 
 SQL = [
     'CREATE TABLE IF NOT EXISTS TABLE_TEST (I INTEGER)',
-    'CREATE TABLE IF NOT EXISTS TABLE_TEST (I INTEGER)',   # shows warnings logged
+    'CREATE TABLE IF NOT EXISTS TABLE_TEST (I INTEGER)',  # shows warnings logged
     'INSERT INTO TABLE_TEST VALUES (0)',
     'CREATE TABLE IF NOT EXISTS TABLE_TEST2 (I INTEGER)',
     'DROP TABLE TABLE_TEST',
@@ -16,6 +18,7 @@ SQL = [
 
 with DAG(
     dag_id='dag_with_postgres_operator_v01',
+    degault_args = default_args,
     start_date=datetime(2022,3,3),
     catchup=False,
     schedule_interval='0 0 * * *'
